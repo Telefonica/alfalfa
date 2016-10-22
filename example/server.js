65fd6791c182b40abeda6f725a51ce3d9415976c
Javier Mendiara Cañardo <javier.mendiaracanardo@telefonica.com>
2016-10-22 08:30:17 +0200

    [[CHORE]] Update example

--- example/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const http = require('http');
-const app = require('./app');
-
-// Create your http server that you are exposing, and add the logic (express app in this example)
-let server = http.createServer(app);
-
-module.exports = server;
